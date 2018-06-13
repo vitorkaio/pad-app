e5565afd169660f2d23ef8e4839059ea0d1a81b8
Vítor <vitorcaiodepaula@gmail.com>
2018-06-13 19:52:32 -0300

    push

--- App.js
+++ App.js
@@ -1,21 +1,11 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Navigation } from 'react-native-navigation';
+import HomeScreen from './src/screens/Home/Home';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Home</Text>
-      </View>
-    );
+Navigation.registerComponent('dontpad.HomeScreen', () => HomeScreen);
+
+Navigation.startSingleScreenApp({
+  screen: {
+    screen: 'dontpad.HomeScreen',
+    title: 'Home'
   }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
 });
